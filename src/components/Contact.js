f4871e78186f117c18e3c5b1a885c92b18cc5469
github username <github email address>
2019-04-06 08:11:35 -0400

    added a contact formn that does nothing yet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,15 +7,38 @@ export default class Contact extends Component {
     $('.navsubtitle').text("contact me")
   }
 
+  // emailForm = () => {
+  //   (function(){
+  //      emailjs.init("user_u8Qhayu3SqsZiP9k15W3w");
+  //   })();
+  //   window.onload = function() {
+  //       document.getElementById('contact-form').addEventListener('submit', function(event) {
+  //           event.preventDefault();
+  //           this.contact_number.value = Math.random() * 100000 | 0;
+  //           emailjs.sendForm('contact_service', 'contact_template', this);
+  //       });
+  //   }
+  // }
+
   render() {
     return (
       <div className="contactcontainer">
-        <div className="details">Contact form coming soon</div>
+        <form id="contact-form">
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="contact"></textarea>
+          <input type="submit" value="Send" />
+        </form>
       </div>
     );
   };
 
   componentDidMount(){
+    // {this.emailForm()}
     {this.changeHeader()}
   }
 
