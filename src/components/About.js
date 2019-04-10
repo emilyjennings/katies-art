a7f5ca45ff7b1ebf2ea882246d4cf7e2d9c36c77
github username <github email address>
2019-04-09 22:47:51 -0400

    some styling changes

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,9 @@ export default class About extends Component {
         <div className="aboutimage">
           <img src={katie} alt="" />
         </div>
-        <div className="bio">Since receiving her BFA from the Corcoran School of the Arts and Design, Katie Macyshyn has taught art and empowered self expression through workshops, creative group activities, and high camp multimedia rituals. In addition to her new media and art education practice, Macyshyn has oil painted for two decades. Her paintings utilize vibrant half-tones and light to create realistic portraits with a touch of whimsy. She has been included in gallery exhibitions in the Washington DC region including group shows at VisArts, IA&A at Hillyer, and the Corcoran Gallery of Art. She is from Toms River, NJ.</div>
+        <div className="bio">
+          Katie Macyshyn uses high resolution photographs to make realistic and colorful portrait paintings made to order with customized sizes, backgrounds and colors. Macyshyn has oil painted for two decades. Her paintings utilize vibrant half-tones and light to create realistic portraits with a touch of whimsy. She has been included in gallery exhibitions in the Washington DC region including group shows at VisArts, IA&A at Hillyer, and the Corcoran Gallery of Art. She is from Toms River, NJ.
+        </div>
       </div>
     );
   };
