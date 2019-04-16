1369a63b6bb4f64cc0c52a8ebdb10ea490691296
github username <github email address>
2019-04-16 16:02:26 -0400

    added id for photos so they can be cropped

--- src/components/GalleryCards.js
+++ src/components/GalleryCards.js
@@ -25,83 +25,94 @@ export default class GalleryCards extends Component {
         link: portrait1,
         desc: "11x14 custom oil paint pet portrait, bust",
         price: "$225",
-        name: "/portrait1"
+        name: "/portrait1",
+        id: "one"
       },
       image2: {
         link: image2,
         desc: "11x14 custom oil paint pet portrait, full figure",
         price: "$275",
-        name: "/portrait2"
+        name: "/portrait2",
+        id: "two"
       },
       image3: {
         link: image3,
         desc: "11x14 double pet oil portrait",
         price: "$325",
-        name: "/portrait3"
+        name: "/portrait3",
+        id: "three"
       },
       image4: {
         link: image4,
         desc: "14x11 double oil portrait",
         price: "$350",
-        name: "/portrait4"
+        name: "/portrait4",
+        id: "four"
       },
       image5: {
         link: image5,
         desc: "20x16 oil portrait 3 figures",
         price: "$450",
-        name: "/portrait5"
+        name: "/portrait5",
+        id: "five"
       },
       image6: {
         link: image6,
         desc: "Mural",
         price: "$1000",
-        name: "/murals"
+        name: "/murals",
+        id: "six"
       },
       image7: {
         link: image7,
         desc: "12x9 Watercolor home portrait",
         price: "$65",
-        name: "/homes"
+        name: "/homes",
+        id: "seven"
       },
       image8: {
         link: image8,
         desc: "16x20 oil portrait - bust",
         price: "$300",
-        name: "/portrait6"
+        name: "/portrait6",
+        id: "eight"
       },
       image9: {
         link: image9,
         desc: "16x20 oil portrait - full figure",
         price: "$350",
-        name: "/portrait7"
+        name: "/portrait7",
+        id: "nine"
       },
       image10: {
         link: image10,
         desc: "San Miguel de Allende oil painting",
         price: "$350",
-        name: "/sanmiguel"
+        name: "/sanmiguel",
+        id: "ten"
       },
       image11: {
         link: image11,
         desc: "16x20 oil portrait - half figure",
         price: "$325",
-        name: "/portrait8"
+        name: "/portrait8",
+        id: "eleven"
       },
       image12: {
         link: image12,
         desc: "16x20 oil pet portrait, bust",
         price: "$275",
-        name: "/portrait9"
+        name: "/portrait9",
+        id: "twelve"
       },
       image13: {
         link: image13,
         desc: "16x20 oil pet portrait - full figure",
         price: "$325",
-        name: "/portrait10"
+        name: "/portrait10",
+        id: "thirteen"
       },
-
     }
-    // , image3, image4, image5, image6, image7, image8, image9, image11, image10, image12, image13}
   }
 
   changeHeader = () => {
@@ -117,7 +128,7 @@ export default class GalleryCards extends Component {
     return Object.values(this.state.images).map(image =>
       <div className="clearfix">
         <Link to={image.name}><div className="cardborder">
-          <div className="imageborder">
+          <div className="imageborder" id={image.id}>
             <img src={image.link} alt="" />
           </div>
           <div className="caption">{image.desc}</div>
