11a8b1367074a47eaa64f41ebab8da3432c5df0d
github username <github email address>
2019-04-17 10:21:43 -0400

    added ping every 30 seconds and the favicon

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 
-// var template_params = {
-//    "user_email": "user_email_value",
-//    "contact_number": "contact_number_value",
-//    "contact": "contact_value"
-// }
-//
-// var service_id = "default_service";
-// var template_id = "contact_form";
-// emailjs.send(service_id, template_id, template_params);
+var http = require("http");
+setInterval(function() {
+    http.get("http://katie-paints-portraits.herokuapp.com");
+}, 300000);
