26432cd5623ce33f7b20e2637d9da938769f332e
github username <github email address>
2019-05-12 17:35:25 -0400

    changed form to netlify regs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import $ from 'jquery'
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 export default class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  // Code for the contact form submission given by Netlify documentation
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
 
 // changes the header subtitle
@@ -21,15 +47,29 @@ export default class Contact extends Component {
 
 // contact form as per code given by EmailJS, thwe library used for this form
   render() {
+    const { name, email, message } = this.state;
     return (
       <div className="contactcontainer">
         <div className="formcaption">Leave me your name and what you're looking for! I'll be in touch soon.</div>
-        <form id="contact-form" >
-          <input type="hidden" name="contact_number" />
-          email: <input type="email" name="user_email" className="email_field" />
-          name: <input type="text" name="user_name" className="name_field" />
-          message: <textarea name="text" className="message_field" />
-          <input type="submit" value="Send" className="submit" />
+        <form id="contact-form" onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" value={message} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button id="submit" type="submit">Send</button>
+          </p>
         </form>
       </div>
     );
