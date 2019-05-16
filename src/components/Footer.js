23dffe4f0a7f5cfb2f6f184f31ca3bf897133b39
github username <github email address>
2019-05-15 22:10:08 -0400

    changed link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <a href="https://www.etsy.com/shop/KatiePaintsPortraits?ref=simple-shop-header-name&listing_id=590465438"><i class="fab fa-etsy"></i></a>
           <a href="https://www.facebook.com/katiepaintsportraits/"><i class="fab fa-facebook-square"></i></a>
         </div>
-        <div className="emily"><a href="https://emilyjennings.github.io">Made by Emily Jennings</a></div>
+        <div className="emily"><a href="https://www.this-is-emily.com">Made by Emily Jennings</a></div>
       </div>
     );
 
